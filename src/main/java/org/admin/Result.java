f6054f2bb989266b3837deb4ea0b39a1b6665af0
Elias Morello <eliasxmorello@aol.com>
2024-12-27 05:59:32 +0100

    Remove unnecessary code.

--- src/main/java/org/admin/Result.java
+++ src/main/java/org/admin/Result.java
@@ -146,43 +146,5 @@ public class Result {
             e.printStackTrace();
             return null;
         }
-        /*double[] cpCounts = new double[input.size()];
-        for(int i = 0; i < input.size(); i++)
-            cpCounts[i] = input.get(i).cp;
-
-        double[] inputRatings = new double[input.size()];
-        for(int i = 0; i < input.size(); i++)
-            inputRatings[i] = input.get(i).rating;
-
-        MPSolver solver = new MPSolver("ILP_Solver", MPSolver.OptimizationProblemType.CBC_MIXED_INTEGER_PROGRAMMING);
-
-        int variableCount = input.size();
-        MPVariable[] variables = new MPVariable[variableCount];
-        for(int i = 0; i < variableCount; i++)
-            variables[i] = solver.makeIntVar(0.0, 1.0, "x" + (i + 1));
-
-        double[][] coefficients = { cpCounts };
-        double[] bounds = {39};
-
-        for(int i = 0; i < coefficients.length; i++) {
-            MPConstraint constraint = solver.makeConstraint(0, bounds[i], "Constraint" + (i + 1));
-            for(int j = 0; j < variableCount; j++)
-                constraint.setCoefficient(variables[j], coefficients[i][j]);
-        }
-
-        MPObjective target = solver.objective();
-        for(int i = 0; i < variableCount; i++) {
-            target.setCoefficient(variables[i], inputRatings[i]);
-        }
-        target.setMaximization();
-
-        MPSolver.ResultStatus resultStatus = solver.solve();
-        if (resultStatus == MPSolver.ResultStatus.OPTIMAL) {
-            System.out.println("Optimal solution found:");
-            for (int i = 0; i < variableCount; i++)
-                System.out.printf("x%d = %f%n", i + 1, variables[i].solutionValue());
-            System.out.printf("Optimal objective value: %f%n", target.value());
-        } else
-            System.out.println("No optimal solution found.");*/
     }
 }
